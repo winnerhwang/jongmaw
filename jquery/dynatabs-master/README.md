44a3fd2169369fe3134a1ee20772fe7f0415ba98
Winner Huang <winnerhwang@hotmail.com>
2016-03-03 10:57:16 +0800

    First upload
    Jquery w2ui jtable dynatabs 
    w2ui.locale.zh-tw.json

---
+++ jquery/dynatabs-master/README.md
@@ -0,0 +1,6 @@
+JQuery Dyna Tabs
+================
+
+JQuery DynaTas is a simple solution for people who want to use dynamic closeable tabs in their web pages. Every attempt has been made to keep it simple and quick.
+
+Please visit, http://www.thedazzlersinc.com/source/dynatabs/ for tutorials and how-to's
---
+++ jquery/jtable/localization/jquery.jtable.zh-TW.js
@@ -0,0 +1,30 @@
+/*
+    jTable localization file for 'Chinese' language.
+    Author: monkeycraps
+*/
+(function ($) {
+
+    $.extend(true, $.hik.jtable.prototype.options.messages, {
+        serverCommunicationError: '服務器請求錯誤。',
+        loadingMessage: '加載中...',
+        noDataAvailable: '沒有數據！',
+        addNewRecord: '+ 新建',
+        editRecord: '編輯',
+        areYouSure: '確定？',
+        deleteConfirmation: '確定刪除這行數據？',
+        save: '保存',
+        saving: '保存中',
+        cancel: '取消',
+        deleteText: '刪除',
+        deleting: '刪除中',
+        error: '錯誤',
+        close: '關閉',
+        cannotLoadOptionsFor: '無法加載對象屬性 {0}',
+        pagingInfo: '顯示 {0} 至 {1} 共 {2}',
+        canNotDeletedRecords: '刪除失敗 {0} 至 {1}!',
+        deleteProggress: '正在刪除 {0} 至 {1} 記錄, 進行中...',
+        pageSizeChangeLabel: '筆數', //New. Must be localized.
+        gotoPageLabel: '跳頁至' //New. Must be localized.
+    });
+
+})(jQuery);
